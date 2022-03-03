671813513aee657fe8bf7383fe93db773cef17bd
hexuekai <hexuekai@corp.netease.com>
2022-03-03 16:45:19 +0800

    add

---
+++ simple/src/main/java/com/example/simple/SimpleApplication.java
@@ -0,0 +1,15 @@
+package com.example.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Date 2022/3/3 16:42
+ * @Description
+ */
+@SpringBootApplication
+public class SimpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class);
+    }
+}
